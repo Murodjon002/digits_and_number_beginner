27fdc46c8841d613d638aec4baaf77855dcdc2a3
Murodjon Abdinabiyev Azamat o'g'li <89096497+Murodjon002@users.noreply.github.com>
2021-12-25 20:08:35 +0500

    length

--- get_length.py
+++ get_length.py
@@ -1,5 +1,10 @@
 #get number of digits in an int?
 def get_length(num):
+    a=num//1
+    b=num//10
+    c=num//100
+    d=num//1000
+    e=num//10000
     """
     Get length of integer
 
@@ -10,6 +15,6 @@ def get_length(num):
         int: length of integer
     """
     # return number of digits in integer
-    return 
+    return a+b+c+d+e
 
  
